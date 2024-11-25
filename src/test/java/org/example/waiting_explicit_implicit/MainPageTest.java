cfa7558565ff8b942880efc8367206bcc6823069
Pro100Dima1 <dima.sinkin@yandex.ru>
2024-11-25 21:19:21 +0300

    Add Exp, Imp Waiting with options PageLoadStrategy.EAGER

--- src/test/java/org/example/waiting_explicit_implicit/MainPageTest.java
+++ src/test/java/org/example/waiting_explicit_implicit/MainPageTest.java
@@ -1,26 +1,26 @@
 package org.example.waiting_explicit_implicit;
 
 import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MainPageTest {
     private WebDriver driver;
-    private MainPage mainPage;
 
     @BeforeEach
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
         // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
@@ -28,14 +28,15 @@ public class MainPageTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.jetbrains.com/");
 
-        mainPage = new MainPage(driver);
     }
+
     @AfterEach
     public void tearDown() {
         driver.quit();
     }
+
     @AfterAll
-    public static void endingText(){
+    public static void endingText() {
         System.out.println("Тесты завершены");
     }
 
@@ -43,12 +44,10 @@ public class MainPageTest {
     @DisplayName("Тест по проверке кнопки Visible After 5 Seconds")
     public void visibleAfterSeconds() {
         driver.get("https://demoqa.com/dynamic-properties");
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(6));
-
-        driver.findElement(By.cssSelector("#visibleAfter").click());
-        wait.until(ExpectedCondition.elementToBeClickable(By.cssSelector("#visibleAfter")));
-        WebElement but = new driver.findElement(By.cssSelector("#visibleAfter"));
-        assertTrue(but.isDisplayed(), "Нет нужной кнопки");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        WebElement but = driver.findElement(By.cssSelector("#visibleAfter"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#visibleAfter")));
+        assertTrue(but.isEnabled(), "После 5-ти секунд неактивна");
 
 
     }
